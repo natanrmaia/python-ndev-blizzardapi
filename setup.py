b5c74f45f2f987754d996b260ac799a8377a807e
Natanael Maia <contato@natanael.dev.br>
2023-03-10 11:42:42 -0300

    Ajuste setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,14 @@ setup(
     author="natanrmaia",
     author_email="contato@natanael.dev.br",
     description="Python module created to integrate your Python project with the Blizzard API",
-    long_description="Python module created to integrate your Python project with the Blizzard API",
-    long_description_content_type="text/markdown",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     url="https://github.com/natanrmaia/python-ndev-blizzardapi",
+    project_urls={
+        'Documentation': 'https://python-blizzardapi.natanael.dev.br/en/latest/',
+        'Source': 'https://github.com/natanrmaia/python-ndev-blizzardapi',
+        'Bug Reports': 'https://github.com/natanrmaia/python-ndev-blizzardapi/issues',
+    },    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
