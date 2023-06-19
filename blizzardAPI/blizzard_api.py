0a54b19453fe611d8dbb830f160be5ebcca5b991
Natanael Maia <contato@natanael.dev.br>
2023-06-18 23:11:20 -0300

    Adjust Supported APIs

--- blizzardAPI/blizzard_api.py
+++ blizzardAPI/blizzard_api.py
@@ -12,5 +12,3 @@ class BlizzardAPI:
 
         self.bnet   = BattleNetAPI(client_id, client_secret)
         self.wow    = WoWAPI(client_id, client_secret)
-        # self.sc2    = SC2API(client_id, client_secret)
-        # self.d3     = D3API(client_id, client_secret)
