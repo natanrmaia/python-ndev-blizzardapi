1ab08c2cb50bce4f87218271bcee69dd42b6ff02
Natanael Maia <contato@natanael.dev.br>
2023-06-21 23:21:50 -0300

    Doc improve

--- blizzardAPI/tests/world_of_warcraft/game_data/test_token.py
+++ blizzardAPI/tests/world_of_warcraft/game_data/test_token.py
@@ -1,15 +1,20 @@
 from blizzardAPI.world_of_warcraft.game_data.token import Token
-import pytest, os
 
 class TestToken:
 
-    def test_get_token_index(self, api_settings):
+    def test_get_token_index(self, api_settings : dict):
         """
         This function tests the get_token_index function from the Token class.
 
         This function will test the following:
             - If the function returns a dict.
             - If the dict contains the key 'price'.
+
+        Args:
+            api_settings (dict): A dictionary containing the api args required for test.
+
+        Returns:
+            bool: A boolean value indicating if the test passed.
 
         Returns:
             A boolean value indicating if the test passed.
