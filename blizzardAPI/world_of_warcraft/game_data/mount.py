7ea254fa8276523214dcb58eeb17d99693585808
Natanael Maia <contato@natanael.dev.br>
2023-06-29 20:31:15 -0300

    Added PyTest for WoW Mount API

--- blizzardAPI/world_of_warcraft/game_data/mount.py
+++ blizzardAPI/world_of_warcraft/game_data/mount.py
@@ -1,21 +1,17 @@
-from typing import Dict, Optional
+from typing import Dict, Any
 from ...api import API
 
 class Mount(API):
-    
+
     def __init__(self, client_id, client_secret):
         super().__init__(client_id, client_secret)
 
-    def get_mounts_index(self, region: Optional[str], locale: Optional[str]) -> Dict:
+    def get_mounts_index(self, **kwargs: Any) -> Dict:
         """
         This function will return the index of mounts from the API.
 
         Requested API:
             /data/wow/mount/index
-
-        Args:
-            region: The region of the API you want to access.
-            locale: The locale of the API you want to access.
 
         Returns:
             A dictionary of the mounts index.
@@ -25,12 +21,13 @@ class Mount(API):
 
         query_params = {
             'namespace': 'static',
-            'locale': locale,
         }
 
-        return super().get_api(region, api, query_params)
+        query_params.update(kwargs)
 
-    def get_mount(self, region: Optional[str], locale: Optional[str], mount_id: int) -> Dict:
+        return super().get_api(api=api, query_params=query_params, kwargs=kwargs)
+
+    def get_mount(self, mount_id: int, **kwargs: Any) -> Dict:
         """
         This function will return the details of a specific mount from the API.
 
@@ -38,8 +35,6 @@ class Mount(API):
             /data/wow/mount/{mountId}
 
         Args:
-            region: The region of the API you want to access.
-            mount_id: The ID of the mount you want to retrieve.
             locale: The locale of the API you want to access.
 
         Returns:
@@ -50,25 +45,18 @@ class Mount(API):
 
         query_params = {
             'namespace': 'static',
-            'locale': locale,
         }
 
-        return super().get_api(region, api, query_params)
+        query_params.update(kwargs)
 
-    def search_mounts(self, region: Optional[str], locale: Optional[str], name_en: Optional[str], order_by: Optional[str], page: Optional[int]) -> Dict:
+        return super().get_api(api=api, query_params=query_params, kwargs=kwargs)
+
+    def get_mount_search(self, **kwargs: Any) -> Dict:
         """
         This function will search for mounts that match the given search query.
 
         Requested API:
             /data/wow/search/mount
-
-        Args:
-            region: The region of the API you want to access.
-            name: The search query to use.
-            locale: The locale of the API you want to access.
-            orderby: The field to order the results by.
-            _page: The page number of the results to return.
-            **kwargs: Additional query parameters to include in the request.
 
         Returns:
             A dictionary of the search results.
@@ -78,16 +66,8 @@ class Mount(API):
 
         query_params = {
             'namespace': 'static',
-            'locale': locale,
         }
 
-        if name_en is not None:
-            query_params['name.en_US'] = name_en
+        query_params.update(kwargs)
 
-        if order_by is not None:
-            query_params['orderby'] = order_by
-
-        if page is not None:
-            query_params['_page'] = page
-
-        return super().get_api(region, api, query_params)
+        return super().get_api(api=api, query_params=query_params, kwargs=kwargs)
