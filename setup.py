ce29b154cb9e876a9c5bbc322d116be4b4c00b86
Natanael Maia <contato@natanael.dev.br>
2024-04-30 10:51:55 -0300

    Nova versão disponível

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-ndev-blizzardapi",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "requests>=2.28.2",
